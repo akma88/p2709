c22b1329ca8a002a450bc574113af5aa30584831
Namet Akmarzhan <nametakmarzhan@MacBook-Air--akma.local>
2026-09-23 20:13:46 +0500

    Initial p2709 project

---
+++ android/app/src/main/kotlin/com/example/p2709/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p2709
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:p2709/register_form_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Regiser form demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RegisterFormPage(),
+    );
+  }
+}

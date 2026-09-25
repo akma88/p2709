5f9fa8d0fa0d386b83c2193005077f012a3ff0ba
Namet Akmarzhan <nametakmarzhan@MacBook-Air--akma.local>
2026-09-25 12:15:41 +0500

    Added new functions

--- lib/register_form_page.dart
+++ lib/register_form_page.dart
@@ -20,6 +20,19 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
   final _passController = TextEditingController();
   final _confirmController = TextEditingController();
 
+  final List<String> _countries = [
+    'Russia',
+    'Ukraine',
+    'Kazakhstan',
+    'Germany',
+    'France',
+  ];
+  String? _selectedCountry;
+
+  final _nameFocus = FocusNode();
+  final _phoneFocus = FocusNode();
+  final _passFocus = FocusNode();
+
   @override
   void dispose() {
     _nameController.dispose();
@@ -28,7 +41,19 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
     _storyController.dispose();
     _passController.dispose();
     _confirmController.dispose();
+    _nameFocus.dispose();
+    _phoneFocus.dispose();
+    _passFocus.dispose();
     super.dispose();
+  }
+
+  void _fieldFocusehange(
+    BuildContext context,
+    FocusNode currentFocus,
+    FocusNode nextFocus,
+  ) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
   }
 
   @override
@@ -42,6 +67,11 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
           padding: EdgeInsets.all(16),
           children: [
             TextFormField(
+              focusNode: _nameFocus,
+              autofocus: true,
+              onFieldSubmitted: (_) {
+                _fieldFocusehange(context, _nameFocus, _phoneFocus);
+              },
               controller: _nameController,
               decoration: InputDecoration(
                 labelText: "Full Name *",
@@ -57,12 +87,16 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                   borderSide: BorderSide(color: Colors.blue, width: 2),
                 ),
               ),
-              validator: (val) =>
-                  val == null || val.isEmpty ? 'Name is required' : null,
+              validator: (value) => _validateName(value!),
             ),
 
             SizedBox(height: 10),
             TextFormField(
+              focusNode: _phoneFocus,
+
+              onFieldSubmitted: (_) {
+                _fieldFocusehange(context, _phoneFocus, _passFocus);
+              },
               controller: _phoneController,
               decoration: InputDecoration(
                 labelText: "Phone Number *",
@@ -80,7 +114,16 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 ),
               ),
               keyboardType: TextInputType.phone,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              inputFormatters: [
+                // FilteringTextInputFormatter.digitsOnly
+                FilteringTextInputFormatter(
+                  RegExp(r'^[()\d-]{1,15}$'),
+                  allow: true,
+                ),
+              ],
+              validator: (value) => _validatePhoneNumber(value!)
+                  ? null
+                  : 'Phone number must be entered as (###)###-#### ',
             ),
 
             SizedBox(height: 10),
@@ -92,6 +135,32 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 icon: Icon(Icons.mail),
               ),
               keyboardType: TextInputType.emailAddress,
+              validator: (value) => _validateEmail(value!),
+            ),
+            SizedBox(height: 10),
+
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                icon: Icon(Icons.map),
+                labelText: "Country?",
+              ),
+              items: _countries.map((country) {
+                return DropdownMenuItem<String>(
+                  value: country,
+                  child: Text(country),
+                );
+              }).toList(),
+              onChanged: (data) {
+                print(data);
+                setState(() {
+                  _selectedCountry = data;
+                });
+              },
+              value: _selectedCountry,
+              // validator: (val) {
+              //   return val == null ? 'Please select a country' : null;
+              // },
             ),
 
             SizedBox(height: 20),
@@ -109,6 +178,7 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
 
             SizedBox(height: 10),
             TextFormField(
+              focusNode: _passFocus,
               controller: _passController,
               obscureText: _hidePass,
               maxLength: 8,
@@ -127,6 +197,7 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 ),
                 icon: Icon(Icons.security),
               ),
+              validator: (value) => _validatePassword(value!),
             ),
 
             SizedBox(height: 10),
@@ -139,6 +210,7 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 hintText: "Confirm the password",
                 icon: Icon(Icons.border_color),
               ),
+              validator: (value) => _validatePassword(value!),
             ),
 
             SizedBox(height: 15),
@@ -156,11 +228,52 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
 
   void _submitForm() {
     if (_formkey.currentState!.validate()) {
+      _formkey.currentState?.save();
+      print('Form is valid');
       print("Form is valid");
       print('Name: ${_nameController.text}');
       print('Phone: ${_phoneController.text}');
       print('Email: ${_emailController.text}');
+      print('Counrty: ${_selectedCountry}');
       print('Story: ${_storyController.text}');
+    } else {
+      print('Form is not valid! Please review and correct');
+    }
+  }
+
+  String? _validateName(String value) {
+    final _nameExp = RegExp(r'^[A-Za-z]+$');
+    if (value == null || value.isEmpty) {
+      return 'Name is required.';
+    } else if (!_nameExp.hasMatch(value)) {
+      return 'Please enter alphabetical characters';
+    } else {
+      return null;
+    }
+  }
+
+  bool _validatePhoneNumber(String input) {
+    final phoneExp = RegExp(r'^\(\d\d\d\)\d\d\d\-\d\d\d\d$');
+    return phoneExp.hasMatch(input);
+  }
+
+  String? _validateEmail(String value) {
+    if (value.isEmpty) {
+      return 'Email can not be empty';
+    } else if (!_emailController.text.contains('@')) {
+      return 'Invalid email address';
+    } else {
+      return null;
+    }
+  }
+
+  String? _validatePassword(String value) {
+    if (_passController.text.length != 8) {
+      return '8 characters required for password';
+    } else if (_confirmController.text != _passController.text) {
+      return "Passsword does not match";
+    } else {
+      return null;
     }
   }
 }
